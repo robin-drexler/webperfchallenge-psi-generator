dcf5293b43fa1b757a0dfd92dff34a8ba43c52be
Robin Drexler <drexler.robin@gmail.com>
2016-11-16 17:15:50 +0100

    chore: add debugging output

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ const crawlTopSites = require('./lib/crawl-top-sites');
 const runPageSpeed = require('./lib/run-page-speed');
 const howManySites = process.argv[2] || 3;
 
+// console.error for debug output, so the resulting json is still intact
+// since it's written to stdout
+console.error('Starting');
+
 crawlTopSites().then((urls) => {
     urls = urls.slice(0, howManySites);
+    console.error(`Will check ${urls.length} urls`);
+
     return runPageSpeed(urls);
 }).then((urlsWithScore) => {
     console.log(JSON.stringify((urlsWithScore)));
