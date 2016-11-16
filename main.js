ae7baf455eb969982e881ebe514ceefd7cc39ea3
Robin Drexler <drexler.robin@gmail.com>
2016-11-16 15:54:29 +0100

    feat(app): allow to generate psi score for arbritary number of top sites
    only limited by count of websites in the wikipedia list
    which is >100

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const crawlTopSites = require('./crawl-top-sites');
-
+const runPageSpeed = require('./run-page-speed');
+const howManySites = process.argv[2] || 3;
 
 crawlTopSites().then((urls) => {
-    console.log(urls);
+    urls = urls.slice(0, howManySites);
+    return runPageSpeed(urls);
+}).then((urlsWithScore) => {
+    console.log(JSON.stringify((urlsWithScore)));
 });
---
+++ spec/app-spec.js
@@ -0,0 +1,15 @@
+// this tests the entire application with a smoke tests
+// using only a small amount of websites to save time
+// it ensures that all parts are working correctly together
+describe("smoke test for entire application", () => {
+    it("generates psi score for top websites", (done) => {
+        var execSync = require('child_process').execSync;
+        const output = execSync('node main.js 2').toString();
+        const result = JSON.parse(output);
+        expect(result.length).toEqual(2);
+
+        expect(result[0].url).toContain('http');
+        expect(result[0].score).toEqual(jasmine.any(Number));
+        done();
+    });
+});
